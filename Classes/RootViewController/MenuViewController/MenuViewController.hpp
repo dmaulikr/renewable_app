49d93135baf6a42591edf154165d4a37dc276c23
rocabriales <rocabriales@gmail.com>
2013-04-20 18:38:36 -0500

    Create MenuViewController.hpp

---
+++ Classes/RootViewController/MenuViewController/MenuViewController.hpp
@@ -0,0 +1,24 @@
+// File: Classes/RootViewController/MenuViewController/MenuViewController.hpp
+//
+//  Created by Cabriales on 4/20/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController {
+    
+}
+
+
+-(IBAction)Boton1:(id)sender;
+-(IBAction)Boton2:(id)sender;
+-(IBAction)Boton3:(id)sender;
+-(IBAction)Boton4:(id)sender;
+-(IBAction)Boton5:(id)sender;
+-(IBAction)Boton6:(id)sender;
+
+-(IBAction)BotonExplicaRealidad:(id)sender;
+//- (IBAction)launchPlayer:(id)sender;
+
+
+@end
