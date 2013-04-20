6f22fa6ecc22380ea9df3e0ab41e448d83f47b5a
rocabriales <rocabriales@gmail.com>
2013-04-20 18:47:57 -0500

    Create ExplicaRealidad.hpp

---
+++ Classes/RootViewController/MenuViewController/Realidad/ExplicaRealidad.hpp
@@ -0,0 +1,19 @@
+//  File: Classes/RootViewController/MenuViewController/Realidad/ExplicaRealidad.hpp
+//
+//  Created by Cabriales, Castillo y Gomez on 4/20/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExplicaRealidad : UIViewController{
+    
+    IBOutlet UIScrollView *MiScrollView;
+    
+}
+
+
+
+- (IBAction)launchPlayer:(id)sender;
+
+@end
