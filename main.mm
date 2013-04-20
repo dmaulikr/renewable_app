9e3fc0c4faaa147df7e23d2f8567dd5a7f429e87
rocabriales <rocabriales@gmail.com>
2013-04-20 18:48:45 -0500

    Create main.mm

---
+++ main.mm
@@ -0,0 +1,21 @@
+//
+//  main.m
+//
+//  Created by Cabriales, Castillo y Gomez on 4/20/13.
+//
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import <UIKit/UIKit.h>
+
+#import "NLApportaAppDelegate.hpp"
+
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NLApportaAppDelegate class]));
+    }
+} 
